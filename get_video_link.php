b65f19693e1fed37246e88021beedab796f57a0d
shahrukh <shahrukhijaz31@gmail.com>
2020-05-14 19:31:12 +0500

    changes after oauth update

--- get_video_link.php
+++ get_video_link.php
@@ -1,21 +1,33 @@
 <?php
     // $slug="BombasticDepressedAntMVGame";
-    $client_id = "7v4acmi5udki7hj4j1cbacpqdpacd1";
+    $client_id = "47z5cqbe9actvc1s1fz8ttof1h4f3w";
+    $client_secret_key = "3zbtdhhewp6ok19rn5pn420z8efvdj";
+
     $search = $_POST['search'];
     $search = explode("/", $search);
     $search = $search[count($search)-1];
     
-    $slug = explode("?", $search)[0];    
-
-    $url = "https://api.twitch.tv/helix/clips?id=".$slug;
+    $url = "https://id.twitch.tv/oauth2/token?client_id=".$client_id."&client_secret=".$client_secret_key."&grant_type=client_credentials";
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_POST, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Client-ID: '.$client_id)); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $output = curl_exec($ch);
     curl_close($ch);
 
     $output = json_decode($output, true);
+    $access_token = $output['access_token'];    
+    $slug = explode("?", $search)[0];    
+
+    $url = "https://api.twitch.tv/helix/clips?id=".$slug;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 0);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Client-ID: '.$client_id, 'Authorization: Bearer '.$access_token)); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    $output = json_decode($output, true);    
     $thumb_url = $output['data'][0]['thumbnail_url'];
     $slice_point = explode('-preview-', $thumb_url)[0];
    
